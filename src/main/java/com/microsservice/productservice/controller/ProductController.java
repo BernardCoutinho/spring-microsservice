13d02a08619d33f44f30854c4731b816ada70cc0
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2023-12-22 20:58:50 -0300

    Implemented Get method "getAllProducts".

--- src/main/java/com/microsservice/productservice/controller/ProductController.java
+++ src/main/java/com/microsservice/productservice/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.microsservice.productservice.controller;
 
 import com.microsservice.productservice.dto.ProductRequest;
+import com.microsservice.productservice.dto.ProductResponse;
 import com.microsservice.productservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/product")
@@ -17,5 +20,11 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest){
         service.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return service.getAllProducts();
     }
 }
