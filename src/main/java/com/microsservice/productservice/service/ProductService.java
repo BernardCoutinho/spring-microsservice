8fd755cd1690eb4c51c26a5a130872c5e8e09ab3
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2023-12-22 20:57:51 -0300

    ModelMapper definition and getAllProducts method.

--- src/main/java/com/microsservice/productservice/service/ProductService.java
+++ src/main/java/com/microsservice/productservice/service/ProductService.java
@@ -1,12 +1,16 @@
 package com.microsservice.productservice.service;
 
 import com.microsservice.productservice.dto.ProductRequest;
+import com.microsservice.productservice.dto.ProductResponse;
 import com.microsservice.productservice.model.Product;
 import com.microsservice.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -14,6 +18,9 @@ public class ProductService {
 
     @Autowired
     private ProductRepository repository;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     public void createProduct(ProductRequest productRequest){
         Product product = Product.builder()
@@ -24,6 +31,12 @@ public class ProductService {
         repository.save(product);
 
         log.info("Product {} is saved", product.getId());
+    }
+    public List<ProductResponse> getAllProducts(){
+        List<Product> products = repository.findAll();
+        return products.stream()
+                .map(product -> modelMapper.map(product, ProductResponse.class))
+                .toList();
     }
 }
 
